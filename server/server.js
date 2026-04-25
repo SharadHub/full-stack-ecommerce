a2c6b9ede4ebf13c25e8086606a5199cd86b4e14
Sharad Bista <bishalbista737@gmail.com>
2026-04-25 22:42:49 +0545

    feat(server): add Express server with MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// create database connection and also create separate file then import and use that file
+
+mongoose.connect('mongodb+srv://bishal_db_user:2002@cluster-ecom.nodrtol.mongodb.net/')
+.then(() => console.log('MongoDB is connected')).catch(error => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders:[
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials: true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
