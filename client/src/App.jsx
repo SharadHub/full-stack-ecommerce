f272223457ef4847a3f50e9a0580703e5b36bedc
Sharad Bista <bishalbista737@gmail.com>
2026-04-25 22:42:49 +0545

    feat(client): add main App component with routing structure

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin/layout";
+import AdminDashboard from "./pages/admin/dashboard";
+import AdminFeatures from "./pages/admin/features";
+import AdminOrders from "./pages/admin/orders";
+import AdminProducts from "./pages/admin/products";
+
+function App() {
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
